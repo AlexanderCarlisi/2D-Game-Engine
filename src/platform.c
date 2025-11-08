1bce033d8220e2b6c220552ebfddcb6c33a89d97
AlexanderCarlisi <alexandercarlisi0@gmail.com>
2025-11-08 10:27:34 -0500

    add _cast_window function to platform.c for Linux
    Just adds some more safety. The idea of casting a Pointer to a Pointer
    is already pretty forbodden, and will probably be changed at some point

--- src/platform.c
+++ src/platform.c
@@ -133,6 +133,17 @@ bool platform_render(struct WindowConfig* config, float alpha) {
 xcb_atom_t ATOM_WM_PROTOCOLS = XCB_NONE;
 xcb_atom_t ATOM_WM_DELETE_WINDOW = XCB_NONE;
 
+/// @brief attempt to cast WindowConfig to X11Window.
+/// @return X11Window_ptr, or NULL on fail.
+struct X11Window* _cast_window(struct WindowConfig* windowConfig) {
+    if (windowConfig == NULL) {
+        printf("\n>>> _cast_window: NULL Pointer argument <<<\n");
+        return NULL;
+    }
+
+    return (X11Window*) windowConfig;
+}
+
 /// @brief XCB Window Resize Helper function, doesn't flush.
 void _resize_window(struct X11Window* window, struct Aspect* windowSize) {
     if (!window->connection) return;
@@ -215,7 +226,7 @@ struct WindowConfig* platform_new_window(struct WindowConfig config) {
         XCB_COPY_FROM_PARENT,
         window->window,
         window->screen->root,
-        100, 100,
+        0, 0,
         window->config.window_aspect.width,
         window->config.window_aspect.height,
         0,
@@ -233,7 +244,6 @@ struct WindowConfig* platform_new_window(struct WindowConfig config) {
         ATOM_WM_DELETE_WINDOW = _xcb_request(window, "WM_DELETE_WINDOW");
 
     // Now an atom will be sent when user hits X so we can close properly.
-    // TODO: magic numbers
     xcb_change_property(
         window->connection,
         XCB_PROP_MODE_REPLACE,
@@ -256,179 +266,40 @@ struct WindowConfig* platform_new_window(struct WindowConfig config) {
 }
 
 bool platform_set_window_name(struct WindowConfig* config, const char* name) {
-    X11Window* window = (X11Window*) config;
+    X11Window* window = _cast_window(config);
+    if (window == NULL) return false;
+    
     _rename_window(window, name);
     return xcb_flush(window->connection);
 }
 
 bool platform_set_window_size(struct WindowConfig* config, struct Aspect size) {
-    return false;
+    X11Window* window = _cast_window(config);
+    if (window == NULL) return false;
+    
+    _resize_window(window, &size);
+    return xcb_flush(window->connection);
 }
 
 bool platform_set_window_resolution(struct WindowConfig* config, struct Aspect res) {
+    // TODO: 
     return false;
 }
 
 bool platform_update_window(struct WindowConfig* config) {
-    return false;
+    X11Window* window = _cast_window(config);
+    if (window == NULL) return false;
+    return xcb_flush(window->connection);
 }
 
 bool platform_iterate(struct WindowConfig* config) {
+    // TODO: Input Event Handling
     return false;
 }
 
 bool platform_render(struct WindowConfig* config, float alpha) {
+    // TODO: Pass into render.h?
     return false;
 }
-
-// void platform_start() {
-//     struct ApplicationConfig* config = appconfig_get();
-//
-//     // Connect to the X server
-//     int screenNum;
-//     x11_window.connection = xcb_connect(NULL, &screenNum);
-//     if (xcb_connection_has_error(x11_window.connection)) {
-//         printf("Failed to connect to X Server\n");
-//         return;
-//     }
-//
-//     const xcb_setup_t* setup = xcb_get_setup(x11_window.connection);
-//
-//     // Get the desired screen
-//     xcb_screen_iterator_t iter = xcb_setup_roots_iterator(setup);
-//     for (int i = 0; i < screenNum; i++) {
-//         xcb_screen_next(&iter);
-//     }
-//     x11_window.screen = iter.data;
-//
-//     x11_window.window = xcb_generate_id(x11_window.connection);
-//
-//     uint32_t mask = XCB_CW_BACK_PIXEL | XCB_CW_EVENT_MASK;
-//     uint32_t values[2] = {
-//         x11_window.screen->black_pixel,
-//         XCB_EVENT_MASK_EXPOSURE |
-//         XCB_EVENT_MASK_KEY_PRESS |
-//         XCB_EVENT_MASK_STRUCTURE_NOTIFY // for resize
-//     };
-//
-//     xcb_create_window(
-//         x11_window.connection,
-//         XCB_COPY_FROM_PARENT,
-//         x11_window.window,
-//         x11_window.screen->root,
-//         100, 100,
-//         config->window_aspect.width,
-//         config->window_aspect.height,
-//         0,
-//         XCB_WINDOW_CLASS_INPUT_OUTPUT,
-//         x11_window.screen->root_visual,
-//         mask, values
-//     );
-//
-//     // Set the window name, dont use platform_name_window, since it flushes
-//     xcb_change_property(
-//         x11_window.connection,
-//         XCB_PROP_MODE_REPLACE,
-//         x11_window.window,
-//         XCB_ATOM_WM_NAME,
-//         XCB_ATOM_STRING,
-//         8,
-//         strlen(config->window_name),
-//         config->window_name
-//     );
-//
-//     xcb_map_window(x11_window.connection, x11_window.window);
-//
-//     // Setup listener for WM_DELETE_WINDOW
-//     // TODO: Theres got to be a more optimal way to do this part
-//     xcb_intern_atom_cookie_t cookie = xcb_intern_atom(x11_window.connection, 0, strlen("WM_PROTOCOLS"), "WM_PROTOCOLS");
-//     xcb_intern_atom_reply_t* reply = xcb_intern_atom_reply(x11_window.connection, cookie, NULL); // THIS FLUSHES ALL PREV REQUESTS
-//     xcb_atom_t wm_protocols = reply->atom;
-//     free(reply);
-//     cookie = xcb_intern_atom(x11_window.connection, 0, strlen("WM_DELETE_WINDOW"), "WM_DELETE_WINDOW");
-//     reply = xcb_intern_atom_reply(x11_window.connection, cookie, NULL);
-//     xcb_atom_t wm_delete_window = reply->atom;
-//     free(reply);
-//
-//     // Now an atom will be sent instead of the App dying when user hits X
-//     xcb_change_property(
-//         x11_window.connection,
-//         XCB_PROP_MODE_REPLACE,
-//         x11_window.window,
-//         wm_protocols,
-//         4, 32, 1, &wm_delete_window
-//     );
-//
-//     xcb_flush(x11_window.connection);
-//
-//     // Initialize renderer and engine
-//     // engine_start(create_linux_renderer(connection, &window));
-//
-//     // Event / Engine loop
-//     while (engine_is_running()) {
-//         xcb_generic_event_t *event;
-//         while ((event = xcb_poll_for_event(x11_window.connection))) {
-//             switch (event->response_type & ~0x80) {
-//                 case XCB_EXPOSE:
-//                     // Could trigger a redraw
-//                     break;
-//                 case XCB_CONFIGURE_NOTIFY: {
-//                     // Window resized
-//                     xcb_configure_notify_event_t* cfg = (xcb_configure_notify_event_t*)event;
-//                     // appconfig_platform_resized_window_px(cfg->width, cfg->height);
-//                     break;
-//                 }
-//                 case XCB_CLIENT_MESSAGE: {
-//                     xcb_client_message_event_t* cm = (xcb_client_message_event_t*) event;
-//                     if (cm->data.data32[0] == wm_delete_window) {
-//                         printf("User clicked X, closing window..\n");
-//                         goto end;
-//                     }
-//                 }
-//                 // case XCB_KEY_PRESS:
-//                 //     // Could handle keys or just exit
-//                 //     printf("Key pressed, exiting...\n");
-//                 //     engine_close();
-//                 //     free(event);
-//                 //     goto end;
-//             }
-//             free(event);
-//         }
-//
-//         engine_tick();
-//     }
-//
-// end:
-//     engine_close();
-//     xcb_disconnect(x11_window.connection);
-// }
-//
-// void platform_resize_window(struct Aspect *windowSize) {
-//     if (!x11_window.connection) return;
-//
-//     xcb_configure_window(
-//         x11_window.connection,
-//         x11_window.window,
-//         XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT,
-//         (uint32_t[]){windowSize->width, windowSize->height}
-//     );
-//     xcb_flush(x11_window.connection);
-// }
-//
-// void platform_name_window(const char *name) {
-//     if (!x11_window.connection) return;
-//
-//     xcb_change_property(
-//         x11_window.connection,
-//         XCB_PROP_MODE_REPLACE,
-//         x11_window.window,
-//         XCB_ATOM_WM_NAME, XCB_ATOM_STRING,
-//         8, strlen(name), name);
-//     xcb_flush(x11_window.connection);
-// }
-//
-// struct X11Window* platform_get_x11_window() {
-//     return &x11_window;
-// }
 
 #endif
