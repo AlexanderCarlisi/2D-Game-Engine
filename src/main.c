38f2c30b838f77a2b8423379564d063d85a8cc6c
Alexander Carlisi <alexandercarlisi0@gmail.com>
2025-11-21 21:12:54 -0500

    update main.c TODOs
    damn i cranked thangs out

--- src/main.c
+++ src/main.c
@@ -1,34 +1,12 @@
-// gcc src/*.c -Iinclude -o 2DGameEngine -lgdi32
-// gcc src/*.c -Iinclude -o 2DGameEngine -lxcb -lxcb-shm -lm
+// WIN: gcc src/*.c -Iinclude -o 2DGameEngine -lgdi32
+// LINUX: gcc src/*.c -Iinclude -o 2DGameEngine -lxcb -lxcb-shm -lm
 
-// TODO: REFACTOR/ADDITION World Handler
-// TODO: ADDITION: GameObject Shapes
 // TODO: ADDITION: GameObject Textures
 // TODO: ADDITION: World Physics, Collisions, CollisionBoxes
 // TODO: ADDITION/REFAC: Error Logging to File.
 
-// TODO: Cleanup GameObject formatting
-// TODO: Refactor World handling of GameObjects and Pool
-// TODO: Refactor size variables with size_t
-// TODO: UNIX impl
-// TODO: CLEANUP, go through everything, lots of refactors, theres gonna be tons of uglyness
-//
-// TODO: Refactor Renderer to not be like a 'object' (maybe)
-// TODO: This will probably never happen. but refactor the entire project to allow Multi-Windowness
-//          Requires a rewrite of appconfig, the renderers, engine, and platform
-
 // CURRENT GOALS:
-//  - Implement Rendering of Shapes / Collision Boxes
-//      - Possibly start implementing the rotation on them.
-//
-//      TODO: NEW STANDARD: TOP LEFT IS (0, 0) for everything,
-//      (0, 0) being the bottom right for vertices of shapes no longer.
-//      It gets confusing, but if everything is just (0, 0) top left like
-//      the framebuffer, life is more simple.
-//
-//      TODO: Update doxys ^, code will work itself out eventually
-//
-//  - Implementing Unix support
+// ROTATION OF SHAPES
 
 #include "engine_options.h"
 #include EO_PROJECT_MAIN
